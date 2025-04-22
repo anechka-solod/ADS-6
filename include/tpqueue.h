7cc743a628bdde9ebed96e25fc4fdab3c77773b7
Анна <sol0dova.anna@yandex.ru>
2025-04-22 21:43:02 +0300

    ads-6

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,7 +4,46 @@
 
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+private:
+    struct Node {
+        T data;
+        Node* next;
+        Node(const T& data) : data(data), next(nullptr) {}
+    };
+
+    Node* head;
+public:
+    TPQueue() : head(nullptr) {}
+    ~TPQueue() {
+        while (head) {
+            pop();
+        }
+    }
+    void push(const T& item) {
+      Node* newNode = new Node(item);
+      if (!head || head->data.prior < item.prior) {
+        newNode->next = head;
+        head = newNode;
+        return;
+      }
+      Node* current = head;
+      while (current->next && current->next->data.prior >= item.prior) {
+        current = current->next;
+      }
+      newNode->next = current->next;
+      current->next = newNode;
+    }
+    T pop() {
+      if (!head) throw "Queue is empty";
+      Node* temp = head;
+      T data = head->data;
+      head = head->next;
+      delete temp;
+      return data;
+    }
+    bool isEmpty() const {
+      return head == nullptr;
+    }
 };
 
 struct SYM {
